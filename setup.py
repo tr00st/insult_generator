53cad3c6ab196be614060d9506d62e85269672cb
James Cheese <trust@tr00st.co.uk>
2015-04-26 18:19:34 -0400

    Initial setup

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name='insultgenerator',
+	version='0.1',
+	packages=['insultgenerator'],
+	author='James Cheese',
+	author_email='trust@tr00st.co.uk',
+	install_requires=['six'],
+)
