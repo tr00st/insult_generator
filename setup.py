680d3e199bc7a1b6e6ebf8165821343396c21030
James Cheese <trust@tr00st.co.uk>
2015-05-10 23:48:14 +0100

    Bumping version to 0.2 for initial unstable release

--- setup.py
+++ setup.py
@@ -1,12 +1,24 @@
 from setuptools import setup
 setup(name='insultgenerator',
-	version='0.1',
+	version='0.2',
 	packages=['insultgenerator'],
+	license='MIT',
 	author='James Cheese',
 	author_email='trust@tr00st.co.uk',
 	install_requires=['six'],
 	test_suite='insultgenerator.tests',
+	description='Random insult generator',
+	url="https://github.com/tr00st/insult_generator",
 	package_data = {
 		'insultgenerator.wordlists': '*.txt',
 	},
+	classifiers = [
+		'Development Status :: 3 - Alpha',
+	    'Programming Language :: Python :: 2.7',
+	    'Programming Language :: Python :: 3',
+	    'Programming Language :: Python :: 3.2',
+	    'Programming Language :: Python :: 3.3',
+	    'Programming Language :: Python :: 3.4',
+	    'License :: OSI Approved :: MIT License',
+	]
 )
